09fb08b4e22f808bd5bfd5b36c68e37debe2ea7c
ArKaNeMaN <arkaneman1@gmail.com>
2024-05-10 18:20:16 +0300

    Fix vite base

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: 'https://amxxmodularecosystem.github.io/vue-cwapi-weapons-migrator/',
 })
